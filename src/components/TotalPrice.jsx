ae5cc39517e029783b1cb0c099bf7e70ee18e799
tanvirtbf <tanver018765@gmail.com>
2024-05-16 16:14:50 +0600

    font-size change

--- src/components/TotalPrice.jsx
+++ src/components/TotalPrice.jsx
@@ -1,21 +1,24 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-const TotalPrice = ({productSize,productPrice,shippingCostPerKg}) => {
-  const [data,setData] = useState()
+const TotalPrice = ({ productSize, productPrice, shippingCostPerKg }) => {
+  const [data, setData] = useState();
+  const [isAllProductHere,setIsAllProductHere] = useState(false)
 
-
-
-    useEffect(()=> {
-      let singleProductShippingCost = shippingCostPerKg / productSize;
-      let result = productPrice + singleProductShippingCost;
-      setData(result)
-    },[productSize,productPrice,shippingCostPerKg])
+  useEffect(() => {
+    let singleProductShippingCost = shippingCostPerKg / productSize;
+    let result = productPrice + singleProductShippingCost;
+    setData(result);
+    if(productPrice==="" && shippingCostPerKg===""&&productSize===""){
+      setIsAllProductHere(false)
+    }else{
+      setIsAllProductHere(true)
+    }
+  }, [productSize, productPrice, shippingCostPerKg]);
   return (
-    <div>
-      {/* <button onClick={handleClick}>Calculate Price</button> */}
-      <p>Total Price : {data}</p>
+    <div style={{margin:'auto'}}>
+      <p style={{fontSize: '1.5rem',fontWeight:'700'}}>Total Price : {isAllProductHere ? data : 0}</p> 
     </div>
-  )
-}
+  );
+};
 
-export default TotalPrice
+export default TotalPrice;
